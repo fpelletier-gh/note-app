452b4a766d760548fbecf200b4a3b4de96c86f9d
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-25 16:13:51 -0400

    updtate gatsby-config and delete old unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,32 +5,17 @@ module.exports = {
     author: `Francis`,
   },
   plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `Notes App`,
-        short_name: `Notes`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color:  `#f7f0eb`,
-        display: `standalone`,
-        icon: `src/images/icon.png`,
+        name: "images",
+        path: "./src/images/",
       },
+      __key: "images",
     },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-sass`,
-        // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
