cb8bed2dbb0861ea3ded372aea5d56c2c6bc5798
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-25 13:26:47 -0400

    remove comment

--- src/components/footer.js
+++ src/components/footer.js
@@ -51,31 +51,3 @@ const Footer = () => {
   )
 }
 export default Footer
-/* <Tooltip hasArrow label="Contact me" placement="top"> */
-/*   <Box> */
-/*     <ActiveLink href="/contact"> */
-/*       <Icon as={BsEnvelope} w={5} h={5} /> */
-/*     </ActiveLink> */
-/*   </Box> */
-/* </Tooltip> */
-/* <Tooltip hasArrow label="Github Profile" placement="top"> */
-/*   <Box> */
-/*     <ActiveLink href="https://github.com/fpelletier-gh"> */
-/*       <Icon as={BsGithub} w={5} h={5} /> */
-/*     </ActiveLink> */
-/*   </Box> */
-/* </Tooltip> */
-/* <Tooltip hasArrow label="Portfolio" placement="top"> */
-/*   <Box> */
-/*     <ActiveLink href="https://francispelletier.ca"> */
-/*       <Icon as={BsPersonCircle} w={5} h={5} /> */
-/*     </ActiveLink> */
-/*   </Box> */
-/* </Tooltip> */
-/* <Tooltip hasArrow label="This Project Repository" placement="top"> */
-/*   <Box> */
-/*     <ActiveLink href="https://github.com/fpelletier-gh/web-development-notes"> */
-/*       <Icon as={BsCodeSlash} w={5} h={5} /> */
-/*     </ActiveLink> */
-/*   </Box> */
-/* </Tooltip> */
