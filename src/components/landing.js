45d3073ea4dc3b2f6678f95f063a01e1d2ce5198
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-25 11:48:05 -0400

    update visual for landing components

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 
-const IndexPage = ({handleLandingBtn}) => (
+const IndexPage = ({ handleLandingBtn }) => (
   <Layout>
-    <div className='main-text'>
+    <div className="main-text">
       <h1>Note App</h1>
       <h3>A simple open source note taking app</h3>
-      <button className='main-btn' onClick={handleLandingBtn}>Note</button>
+      <button className="main-btn" onClick={handleLandingBtn}>
+        Welcome
+      </button>
     </div>
   </Layout>
 )
