6d3004e891106848f320597e00a9574e16c1a23e
Francis Pelletier <francispelletier2@hotmail.com>
2022-08-25 12:40:51 -0400

    change the delete note div to a button

--- src/components/noteDisplay.js
+++ src/components/noteDisplay.js
@@ -1,17 +1,19 @@
 import React from "react"
 
 const noteDisplay = ({ search, savedNote, handleDeleteNote }) => {
-  let filteredNote = savedNote.filter(note =>
+  let filteredNote = savedNote.filter((note) =>
     note.title.toLowerCase().includes(search.toLowerCase())
   )
   return (
     <>
       {filteredNote.reverse().map(({ title, note, id }) => (
         <div className="note-container" key={id} name={id}>
+          <button className="link-button" onClick={handleDeleteNote} name={id}>
+            <h5 className="delete-btn" name={id}>
+              X
+            </h5>
+          </button>
           <h4>{title}</h4>
-          <h5 className="delete-btn" onClick={handleDeleteNote} name={id}>
-            X
-          </h5>
           <textarea value={note} readOnly></textarea>
         </div>
       ))}
